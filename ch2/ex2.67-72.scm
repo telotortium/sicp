a117cad288dae54f6e67df96bef743a57cbe1027
Robert Irelan <rirelan@gmail.com>
2014-07-07 08:19:57 -0700

    Exercise 2.69-70

--- ch2/ex2.67-72.scm
+++ ch2/ex2.67-72.scm
@@ -130,16 +130,87 @@
 ;;; advantage of the fact that we are using an ordered set representation.)
 
 (define (successive-merge leaf-set)
-  (let proc ((leaf-set leaf-set)
-             (result-tree '()))
-    ;; Length 0 or 1 leaf-set
-    (if (null? leaf-set)
-      result-tree
-      (let ((left (car leaf-set))
-            (right (cadr leaf-set)))
-        (if (leaf? left)
-          (proc (cddr leaf-set)
-                (make-code-tree left right))
-          (make-code-tree (proc (cddr leaf-set) '())))))))
+  (let proc ((tree-set leaf-set))
+   (if (or (null? tree-set) (null? (cdr tree-set)))
+     tree-set
+     (let ((min1 (car tree-set))
+           (min2 (cadr tree-set))
+           (rest (cddr tree-set)))
+       (proc (adjoin-set (make-code-tree min1 min2) rest))))))
+
+;;; }}}
+
+;;; Exercise 2.70 {{{
+;;;
+;;; The following eight-symbol alphabet with associated relative frequencies
+;;; was designed to efficiently encode the lyrics of 1950s rock songs. (Note
+;;; that the ``symbols'' of an ``alphabet'' need not be individual letters.)
+
+;; A	2	NA	16
+;; BOOM	1	SHA	3
+;; GET	2	YIP	9
+;; JOB	2	WAH	1
+
+;;; Use generate-huffman-tree (exercise 2.69) to generate a corresponding
+;;; Huffman tree, and use encode (exercise 2.68) to encode the following
+;;; message:
+
+;; Get a job
+;;
+;; Sha na na na na na na na na
+;;
+;; Get a job
+;;
+;; Sha na na na na na na na na
+;;
+;; Wah yip yip yip yip yip yip yip yip yip
+;;
+;; Sha boom
+
+;;; How many bits are required for the encoding? What is the smallest number of
+;;; bits that would be needed to encode this song if we used a fixed-length
+;;; code for the eight-symbol alphabet?
+
+(define rock-tree (generate-huffman-tree
+                    '((A 2) (BOOM 1) (GET 2) (JOB 2)
+                            (NA 16) (SHA 3) (YIP 9) (WAH 1))))
+
+(define rock-message '(GET A JOB
+                           SHA NA NA NA NA NA NA NA NA
+                           GET A JOB
+                           SHA NA NA NA NA NA NA NA NA
+                           WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP
+                           SHA BOOM))
+
+(define rock-encoded (encode rock-message rock-tree))
+; => (0 1 1 1 1 1 0 1 1 0 0 0 1 1 1 1 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 0 1 1 0 0 0 1 1 1 1 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 1 1 0 0 1 1 0 1 1)
+
+(length rock-encoded)
+; => 120
+
+(* 3 (length rock-message))
+; => 108
+
+;;; }}}
+
+;;; Exercise 2.71 {{{
+;;;
+;;; Suppose we have a Huffman tree for an alphabet of n symbols, and that the
+;;; relative frequencies of the symbols are 1, 2, 4, ..., 2n-1. Sketch the tree
+;;; for n=5; for n=10. In such a tree (for general n) how many bits are
+;;; required to encode the most frequent symbol? the least frequent symbol?
+
+;;; }}}
+
+;;; Exercise 2.72 {{{
+;;;
+;;; Consider the encoding procedure that you designed in exercise 2.68. What is
+;;; the order of growth in the number of steps needed to encode a symbol? Be
+;;; sure to include the number of steps needed to search the symbol list at
+;;; each node encountered. To answer this question in general is difficult.
+;;; Consider the special case where the relative frequencies of the n symbols
+;;; are as described in exercise 2.71, and give the order of growth (as
+;;; a function of n) of the number of steps needed to encode the most frequent
+;;; and least frequent symbols in the alphabet.
 
 ;;; }}}
