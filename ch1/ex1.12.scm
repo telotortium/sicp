ff08a5b419bc7507820ad043049872f3f040a53e
Robert Irelan <rirelan@gmail.com>
2015-05-23 19:31:00 -0700

    Section 1.2.2 (excluding Exercise 1.13)

--- ch1/ex1.12.scm
+++
@@ -1,9 +0,0 @@
-;; Pascal's triangle
-(define (choose n k)
-  (cond
-    ((or (< n 0) (< k 0) (> k n))
-     0)
-    ((or (= k 0) (= k n))
-     1)
-    (else (+ (choose (- n 1) (- k 1))
-             (choose (- n 1) k)))))
